8f4b889d54cf2bf9505cae1b3e5b4b24afdb27ad
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-16 02:09:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,7 @@ class TicTacToe
       end
 
       def valid_move?(index)
-            index.between?(0,8) && !position_taken?(@board, index)
+            index.between?(0,8) && !position_taken?(index)
       end
 
       def turn_count
@@ -43,7 +43,7 @@ class TicTacToe
       end
 
       def current_player
-      turn_count(@board) % 2 == 0 ? "X" : "O"
+      turn_count % 2 == 0 ? "X" : "O"
       end
 
       def turn
